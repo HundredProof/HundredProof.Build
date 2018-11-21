72631e43769c4866cd8970cd831a091bcd55abd9
Lawson Caudill <lawson@caudillclan.us>
2018-11-21 16:05:46 -0300

    Initial configuration.

---
+++ AppVeyorBuildScripts/AppVeyorBuildScript-Normal.ps1
@@ -0,0 +1,78 @@
+# PASTE THIS INTO APPVEYOUR - Normal
+
+Write-Host $env:APPVEYOR_JOB_ID
+Write-Host $env:APPVEYOR_API_URL
+
+#######################################################################
+###     Setup variables from Appveyor                               ###
+#######################################################################
+$repoPath = Resolve-Path .
+$buildVersion = $env:appveyor_build_version
+$branchName = $env:appveyor_repo_branch
+$repoName = $env:appveyor_repo_name
+$nugetUrl = $env:nuget_gallery_url
+$nugetKey = $env:nuget_api_key
+$mygetUrl = $env:myget_gallery_url
+$mygetKey = $env:myget_api_key
+
+# normal build specific
+$treatBuildWarningsAsErrors = $env:treatBuildWarningsAsErrors
+$runCodeAnalysis = $env:runCodeAnalysis
+$appveyorNugetUpdateStrategyPrivate = $env:appveyor_nuget_update_strategy_private
+$appveyorNugetUpdateStrategyPublic = $env:appveyor_nuget_update_strategy_public
+
+#######################################################################
+###     Download and dot source tools to use                        ###
+#######################################################################
+NuGet sources add -Name HundredProofMyGet -Source https://www.myget.org/F/hundredproof-nuget/api/v3/index.json
+NuGet sources add -Name NaosMyGet -Source https://www.myget.org/F/naos-nuget/api/v3/index.json
+NuGet sources add -Name ObcMyGet -Source https://www.myget.org/F/obeautifulecode-nuget/api/v3/index.json
+$TempBuildPackagesDir = "../TempTools/packages"
+if (-not (Test-Path $TempBuildPackagesDir)) { md $TempBuildPackagesDir | Out-Null }
+$TempBuildPackagesDir = Resolve-Path $TempBuildPackagesDir
+NuGet install 'Naos.Powershell.Build' -Prerelease -OutputDirectory $TempBuildPackagesDir
+NuGet install 'HundredProof.Build.Packaging' -Prerelease -OutputDirectory $TempBuildPackagesDir
+NuGet install 'HundredProof.Build.Conventions.RepoConfig' -Prerelease -OutputDirectory $TempBuildPackagesDir
+
+$repoConfigFile = (ls $TempBuildPackagesDir -Filter 'RepoConfig.ps1' -Recurse).FullName
+if ($repoConfigFile -eq $null) {
+	ls $TempBuildPackagesDir -Recurse | %{ $_.FullName }
+	throw "Could not find 'RepoConfig.ps1' in $TempBuildPackagesDir"
+}
+&$repoConfigFile -ThrowOnPendingUpdate -PreRelease -RepositoryPath $repoPath
+
+$nuSpecTemplateFile = Join-Path (ls $TempBuildPackagesDir/HundredProof.Build.Packaging.*).FullName 'HundredProofNuSpecTemplate.template-nuspec'
+
+$nugetFunctionsScriptPath = $(ls $TempBuildPackagesDir -Recurse | ?{$_.Name -eq 'NuGet-Functions.ps1'}).FullName
+
+. $nugetFunctionsScriptPath
+
+#######################################################################
+###     Setup NuGet/Artifact scriptblocks                           ###
+#######################################################################
+$nugetScriptblock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+
+   Write-Host "Pushing $fileName to AppVeyor Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $nugetUrl -apiKey $nugetKey
+
+   Write-Host "Pushing $fileName to MyGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $mygetUrl -apiKey $mygetKey
+}
+
+$artifactScriptBlock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+}
+
+#######################################################################
+###     Setup Prerequistes and run build                            ###
+#######################################################################
+&$artifactScriptBlock($nuSpecTemplateFile)
+
+$customMsBuildLogger = 'C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll'
+
+$buildScript = Join-Path (Join-Path (ls $TempBuildPackagesDir/Naos.Powershell.Build.*).FullName 'scripts') 'Build.ps1'
+
+&$buildScript -Version $buildVersion -SourceDirectory (Resolve-Path '.') -BuildPackagesDirectory $TempBuildPackagesDir -PackagesOutputDirectory (Resolve-Path '.') -BranchName $branchName -GalleryUrl $appveyorNugetUrl -PackageUpdateStrategyPrivateGallery $appveyorNugetUpdateStrategyPrivate -PackageUpdateStrategyPublicGallery $appveyorNugetUpdateStrategyPublic -TreatBuildWarningsAsErrors ($treatBuildWarningsAsErrors -eq 'true') -RunCodeAnalysis ($runCodeAnalysis -eq 'true') -NuSpecTemplateFilePath $nuSpecTemplateFile -Authors 'HundredProof' -PushNuGetPackageFile $nugetScriptblock -SaveFileAsBuildArtifact $artifactScriptBlock -CustomMsBuildLogger $customMsBuildLogger -Run
---
+++ AppVeyorBuildScripts/AppVeyorBuildScript-Recipes.ps1
@@ -0,0 +1,80 @@
+# PASTE THIS INTO APPVEYOUR - Recipes
+
+###########################################################
+###       Setup variables from Appveyor                 ###
+###########################################################
+$repoPath = Resolve-Path .
+$branchName = $env:appveyor_repo_branch
+$buildVersion = $env:appveyor_build_version
+
+###########################################################
+###    Download and dot source tools to use             ###
+###########################################################
+NuGet sources add -Name HundredProofMyGet -Source https://www.myget.org/F/hundredproof-nuget/api/v3/index.json
+NuGet sources add -Name NaosMyGet -Source https://www.myget.org/F/naos-nuget/api/v3/index.json
+NuGet sources add -Name ObcMyGet -Source https://www.myget.org/F/obeautifulecode-nuget/api/v3/index.json
+$TempBuildPackagesDir = "../BuildToolsFromNuGet/packages"
+if (-not (Test-Path $TempBuildPackagesDir)) { md $TempBuildPackagesDir | Out-Null }
+$TempBuildPackagesDir = Resolve-Path $TempBuildPackagesDir
+NuGet install HundredProof.Build -OutputDirectory $TempBuildPackagesDir
+
+$nuSpecTemplateFile = Join-Path (Join-Path (ls $TempBuildPackagesDir/HundredProof.Build.*).FullName 'scripts') 'HundredProofNuSpecTemplate.template-nuspec'
+
+$nugetFunctionsScriptPath = $(ls $TempBuildPackagesDir -Recurse | ?{$_.Name -eq 'NuGet-Functions.ps1'}).FullName
+
+. $nugetFunctionsScriptPath
+
+#######################################################################
+###     Setup NuGet/Artifact scriptblocks                           ###
+#######################################################################
+$nugetScriptblock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+   Write-Host "Pushing $fileName to NuGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $env:nuget_gallery_url -apiKey $env:nuget_api_key
+   Write-Host "Pushing $fileName to MyGet Gallery"
+   Nuget-PublishPackage -packagePath $fileName -apiUrl $env:myget_gallery_url -apiKey $env:myget_api_key
+}
+
+$artifactScriptBlock = { param([string] $fileName) 
+   Write-Host "Pushing $fileName to Build Artifacts"
+   Push-AppveyorArtifact $fileName
+}
+
+#######################################################################
+###       Run steps to process recipes repo             ###
+#######################################################################
+
+# if we are in a branch then create a pre-release version for nuget
+$preReleaseSupportVersion = Nuget-CreatePreReleaseSupportedVersion -version $buildVersion -branchName $branchName
+
+# discover the distinct recipes
+$recipes = ls $repoPath | ?{ $_.PSIsContainer } | ?{ -not $_.Name.StartsWith('.') } | %{$_}
+
+# create the nuspec files in place
+$recipes | %{
+	$recipePath = $_.FullName
+	Write-Output ''
+	Write-Output "Creating NuSpec for '$recipePath'"
+	NuGet-CreateRecipeNuSpecInFolder -recipeFolderPath $recipePath -authors 'HundredProof' -nuSpecTemplateFilePath $nuSpecTemplateFile
+}
+
+# push the nuspecs as artifacts
+ls . *.nuspec -Recurse | %{&$artifactScriptBlock($_.FullName)}
+
+# create the nupkgs in place
+$recipes | %{
+	$recipePath = $_.FullName
+	$nuspecFilePath = $(ls $recipePath -Filter "*.$($nuGetConstants.FileExtensionsWithoutDot.Nuspec)").FullName
+	$packageFile = Nuget-CreatePackageFromNuspec -nuspecFilePath $nuspecFilePath -version $buildVersion -throwOnError $true -outputDirectory $recipePath
+}
+
+# push the nupkgs
+$recipes | %{
+	$recipePath = $_.FullName
+	$nuPkgFilePath = $(ls $recipePath -Filter "*.$($nuGetConstants.FileExtensionsWithoutDot.Package)").FullName
+	Write-Output "Pushing package $nuPkgFilePath"
+	&$nugetScriptblock($nuPkgFilePath)
+}
+
+Remove-Item $TempBuildPackagesDir -Recurse -Force
